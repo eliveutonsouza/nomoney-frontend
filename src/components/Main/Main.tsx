93a3feb22710c0595c2ffa5777c34f3b01cea81f
Eliveuton Souza <eliveuton3m@hotmail.com>
2022-04-09 18:48:42 -0300

    create the components Main and Header

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import * as Styled from "./style"
+
+export default function Main() {
+	return (
+		<Styled.Main>
+			<div className="container">
+				<h2>Conteudo</h2>
+			</div>
+		</Styled.Main>
+	)
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
 import React from "react"
+import Header from "../../components/Header"
+import Main from "../../components/Main"
 
 import * as Styled from "./styles"
 
 export default function Home() {
 	return (
-		<>
-	
-		</>
+		<Styled.Container>
+			<Header />
+			<Main />
+		</Styled.Container>
+
 	)
 }
