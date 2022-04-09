cccfec4cc012f889b24604284f751469f610aa12
Eliveuton Souza <eliveuton3m@hotmail.com>
2022-04-09 16:47:33 -0300

    adda assets and reset

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,15 +2,10 @@ import React from "react"
 
 import * as Styled from "./styles"
 
-import { Button } from "../../components"
-
 export default function Home() {
 	return (
 		<>
-			<Styled.Heading1>
-			Olá Mundo!
-			</Styled.Heading1>
-			<Button />
+	
 		</>
 	)
 }
